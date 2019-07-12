d8104335585de01a348b83c7bb790d67527dc763
RaymondWang <raymondwang@u.northwestern.edu>
2019-07-12 16:07:37 -0500

    minor

--- compute_F.py
+++ compute_F.py
@@ -3,6 +3,6 @@ from pymatgen.core.structure import Structure
 
 pattern = xrd.XRDCalculator(wavelength="AgKa")
 
-struct = Structure.from_file("./NaCl.cif", primitive=False, sort=False, merge_tol=0.0)
+struct = Structure.from_file("./data/NaCl.cif", primitive=False, sort=False, merge_tol=0.0)
 pattern = pattern.get_pattern(struct)
 #print(pattern)
