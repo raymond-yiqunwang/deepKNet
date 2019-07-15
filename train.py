836aaae858a071ecba192e0c94c5904bed56db08
RaymondWang <raymondwang@u.northwestern.edu>
2019-07-15 17:36:15 -0500

    working with linear model

--- train.py
+++ train.py
@@ -8,18 +8,16 @@ import argparse
 from model import KNetModel
 import h5py
 
-"""
 parser = argparse.ArgumentParser(description='CNN parameters')
-parser.add_argument('--train_batch_size', type=int, default=64)
-parser.add_argument('--test_batch_size', type=int, default=256)
-parser.add_argument('--filter_size1', type=int, default=5)
-parser.add_argument('--num_filters1', type=int, default=16)
-parser.add_argument('--filter_size2', type=int, default=5)
-parser.add_argument('--num_filters2', type=int, default=36)
-parser.add_argument('--fc_size', type=int, default=128)
-parser.add_argument('--max_step', type=int, default=1000)
+parser.add_argument('--max_npoint', type=int, default=744)
+parser.add_argument('--num_channels', type=int, default=4)
+parser.add_argument('--max_epoch', type=int, default=50, help='Epoch to run [default: 50]')
+parser.add_argument('--batch_size', type=int, default=32, help='Batch Size during training [default: 32]')
+parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate [default: 0.001]')
 FLAGS = parser.parse_args()
-"""
+
+learning_rate = FLAGS.learning_rate
+max_epoch = FLAGS.max_epoch
 
 def read_data(path):
     f = h5py.File(path, 'r')
@@ -32,32 +30,50 @@ def read_data(path):
 class Trainer(object):
     def __init__(self, **kwargs):
         self.KNet_model = KNetModel(**kwargs)
+        self.max_npoint = FLAGS.max_npoint
+        self.num_channels = FLAGS.num_channels
+        self.batch_size = FLAGS.batch_size
 
     def train(self):
-        I_hkl, band_gap = read_data(path="data_test/dataset0.h5")
-        # x shape (None, 744, 4)
-        # y shape (None, 1) 
-        max_points = 744 
-        num_channels = 4
         with self.KNet_model.g_train.as_default():
-            x_train, y_true = self.KNet_model.placeholder_inputs(max_points, num_channels)
+            x_train, y_true = self.KNet_model.placeholder_inputs(self.max_npoint, self.num_channels)
     
-            y_pred = self.KNet_model.get_model(x_train, max_points, num_channels)
+            y_pred = self.KNet_model.get_model(x_train, self.max_npoint, self.num_channels)
     
             loss = self.KNet_model.get_loss(y_pred, y_true)
     
-            optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.5).minimize(loss)
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
     
             with tf.Session() as sess:
                 sess.run(tf.global_variables_initializer())
-                for iter in range(1):
-                    x_batch = I_hkl[10:]
-                    x_batch = x_batch.reshape(x_batch.shape[0], -1)
-                    y_true_batch = band_gap[10:]
-                    feed_dict_train = { x_train: x_batch,
-                                        y_true: y_true_batch}
-                    sess.run(optimizer, feed_dict=feed_dict_train)
+                
+                ops = {
+                    "x_train": x_train,
+                    "y_true": y_true,
+                    "optimizer": optimizer
+                }
+                
+                for epoch in range(max_epoch):
+                    self.train_one_epoch(sess, ops)
+                    if (epoch % 10 == 0):
+                        print("Current epoch: {}".format(epoch))
 
+    def train_one_epoch(self, sess, ops):                    
+        I_hkl, band_gap = read_data(path="data_test/dataset0.h5")
+
+        num_batches = I_hkl.shape[0] // self.batch_size
+
+        for batch_idx in range(num_batches):
+            start_idx = batch_idx * self.batch_size
+            end_idx = (batch_idx+1) * self.batch_size
+
+            x_batch = I_hkl[start_idx:end_idx, :, :]
+            x_batch = x_batch.reshape(x_batch.shape[0], -1)
+            y_true_batch = band_gap[start_idx:end_idx]
+            
+            feed_dict_train = { ops["x_train"]: x_batch,
+                            ops["y_true"]: y_true_batch}
+            sess.run(ops["optimizer"], feed_dict=feed_dict_train)
 
 if __name__ == "__main__":
     trainer = Trainer()
