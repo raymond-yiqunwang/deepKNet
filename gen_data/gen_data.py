608d194f21c6de56dbd485437c15377c2ae8b607
RaymondWang <raymondwang@u.northwestern.edu>
2020-02-21 15:55:43 -0600

    minor

---
+++ gen_data/gen_data.py
@@ -0,0 +1,178 @@
+#! /home/raymondw/.conda/envs/tf-cpu/bin/python
+from pymatgen import MPRester
+from tfrecord_writer import tfRecordWriter
+from pymatgen.core.structure import Structure
+from pymatgen.core.periodic_table import Element
+from collections import defaultdict
+import tensorflow as tf
+import xrd_calculator as xrd
+import pandas as pd
+import os
+import numpy as np
+import random
+
+
+def fetch_materials_data():
+    # properties of interest
+    properties = [ 
+        "material_id", "pretty_formula", "band_gap",
+        "elements", "nsites", "cif", "volume"
+    ]
+    
+    # MaterialsProject API settings
+    my_API_key = "gxTAyXSm2GvCdWer"
+    m = MPRester(api_key=my_API_key)
+    
+    # query data with computed band structures and no warning sign
+    mp_data = m.query(criteria={"band_structure": { "$ne" : None }, "warnings": []}, properties=properties)
+    #mp_data = m.query(criteria={"volume": { "$lt" : 50 }, "band_structure": { "$ne" : None }, "warnings": []}, properties=properties)
+    
+    data_origin = []
+    for entry in mp_data:
+        plist = []
+        for _, val in entry.items():
+            plist.append(val)
+        data_origin.append(plist)
+    
+    data_origin = pd.DataFrame(data_origin, index=None, columns=properties)
+    return data_origin
+
+
+def customize_data(data_origin, wavelength):
+    
+    # show statistics of original data
+    print("Data distribution before customization:")
+    print("Number of instances: {}".format(data_origin.shape))
+    band_gap = data_origin['band_gap']
+    print(" band gap: mean = {:.2f}, median = {:.2f}, standard deviation = {:.2f}, min = {:.2f}, max = {:.2f}"
+          .format(band_gap.mean(), band_gap.median(), band_gap.std(), band_gap.min(), band_gap.max()))
+    nsites = data_origin['nsites']
+    print(" nsites: mean = {:.2f}, median = {:.2f}, standard deviation = {:.2f}, min = {:.2f}, max = {:.2f}"
+          .format(nsites.mean(), nsites.median(), nsites.std(), nsites.min(), nsites.max()))
+    volume = data_origin['volume']
+    print(" volume: mean = {:.2f}, median = {:.2f}, standard deviation = {:.2f}, min = {:.2f}, max = {:.2f}"
+          .format(volume.mean(), volume.median(), volume.std(), volume.min(), volume.max()))
+    # rare elements
+    rare_elements = []
+    elem_dict = defaultdict(int)
+    for elements in data_origin['elements']:
+        for elem in elements:
+            elem_dict[Element(elem).Z] += 1
+    for elem, count in elem_dict.items():
+        if (count < int(0.01*data_origin.shape[0])):
+            rare_elements.append(elem)
+            print("Element No. {} has a count of {}.".format(elem, count))
+
+    # customize data
+    data_custom = data_origin[['band_gap', 'nsites', 'volume', 'cif', 'elements']].copy()
+    drop_instance = []
+    for idx, irow in data_custom.iterrows():
+        if (True in [ (Element(elem).Z in rare_elements) for elem in irow['elements']]) \
+            or (irow['nsites'] > 50) or (irow['volume'] > 800.):
+            drop_instance.append(idx)
+    data_custom = data_custom.drop(drop_instance)
+
+    # show statistics of customized data
+    print("Data distribution after customizaion:")
+    print("Number of instances: {}".format(data_custom.shape))
+    band_gap = data_custom['band_gap']
+    print(" band gap: mean = {:.2f}, median = {:.2f}, standard deviation = {:.2f}, min = {:.2f}, max = {:.2f}"
+          .format(band_gap.mean(), band_gap.median(), band_gap.std(), band_gap.min(), band_gap.max()))
+    nsites = data_custom['nsites']
+    print(" nsites: mean = {:.2f}, median = {:.2f}, standard deviation = {:.2f}, min = {:.2f}, max = {:.2f}"
+          .format(nsites.mean(), nsites.median(), nsites.std(), nsites.min(), nsites.max()))
+    volume = data_custom['volume']
+    print(" volume: mean = {:.2f}, median = {:.2f}, standard deviation = {:.2f}, min = {:.2f}, max = {:.2f}"
+          .format(volume.mean(), volume.median(), volume.std(), volume.min(), volume.max()))
+    # rare elements
+    rare_elements = []
+    elem_dict2 = defaultdict(int)
+    for _, irow in data_custom.iterrows():
+        for elem in irow['elements']:
+            elem_dict2[Element(elem).Z] += 1
+    for elem, count in elem_dict2.items():
+        if (count < int(0.01*data_custom.shape[0])):
+            rare_elements.append(elem)
+            print("Element No. {} has a count of {}.".format(elem, count))
+
+    # number of points
+    npoint_list = []
+    xrdcalc = xrd.XRDCalculator(wavelength=wavelength)
+    for _, irow in data_custom.iterrows():
+        struct = Structure.from_str(irow['cif'], fmt="cif")
+        npoint_list.append(xrdcalc.get_npoint(struct))
+    
+    npoint_array = np.asarray(npoint_list)
+    print(" npoint: mean = {:.2f}, median = {:.2f}, standard deviation = {:.2f}, min = {:.2f}, max = {:.2f}"
+          .format(np.mean(npoint_array), np.median(npoint_array), np.std(npoint_array), np.min(npoint_array), np.max(npoint_array)))
+    """
+    npoint: mean = 2590.45, median = 2319.00, standard deviation = 1623.81, min = 87.00, max = 7339.00
+    """
+
+    return data_custom
+
+
+def write_tfRecord(data, save_dir, wavelength, npoint_cutoff):
+    
+    # split data into train, valid, and test
+    rand_index = list(data.index)
+    random.shuffle(rand_index)
+    train_ratio = 0.97
+    num_train = int(len(rand_index) * train_ratio)
+
+    # init XRD calculator
+    xrdcalc = xrd.XRDCalculator(wavelength=wavelength)
+    
+    # tfRecord writer
+    rw = tfRecordWriter()
+
+    # write train valid test records
+    for mode in ['train', 'valid']:
+        print("writing {} records..".format(mode))
+
+        if (mode == 'train'): index = rand_index[:num_train]
+        else: index = rand_index[num_train:]
+
+        record_file = save_dir + 'pointcloud_{}.tfrecords'.format(mode)
+        with tf.io.TFRecordWriter(record_file) as writer:
+            checkpoint = 0
+            for idx in index:
+                if (checkpoint % 1000 == 0): print(">> checkpoint {}".format(checkpoint))
+                irow = data.loc[idx]
+
+                # generate pointnet
+                struct = Structure.from_str(irow['cif'], fmt="cif")
+                #pointcloud = np.asarray(xrdcalc.get_intensity(struct)).flatten()
+                pointcloud = xrdcalc.get_atomic_form_factor(struct)
+                # tailoring and padding
+                while (len(pointcloud) < npoint_cutoff):
+                    pointcloud.extend(pointcloud)
+                if (len(pointcloud) > npoint_cutoff):
+                    pointcloud = pointcloud[:npoint_cutoff]
+                assert (len(pointcloud) == npoint_cutoff)
+                # output
+                pointcloud = np.asarray(pointcloud).flatten()
+
+                band_gap = irow['band_gap']
+
+                rw.write_feature(writer, pointcloud, band_gap)
+                checkpoint += 1
+            
+            writer.close()
+
+
+if __name__ == "__main__":
+    
+    wavelength = "CuKa"
+    
+    # get data from MaterialsProject
+    data_origin = fetch_materials_data()
+    
+    # customize data and print statistics
+    data_custom = customize_data(data_origin, wavelength)
+    
+    # compute XRD pattern and write to tfRecord
+    #write_tfRecord(data_custom, save_dir="/data2/yiqun/", wavelength=wavelength, npoint_cutoff = 2500)
+    write_tfRecord(data_custom, save_dir="../data/", wavelength=wavelength, npoint_cutoff = 2500)
+
+
---
+++ gen_data/xrd_calculator.py
@@ -0,0 +1,240 @@
+### Copied and modified from pymatgen
+import os
+import json
+import math
+import numpy as np
+from pymatgen.analysis.diffraction.core import AbstractDiffractionPatternCalculator
+from pymatgen.core.structure import Structure
+from pymatgen import MPRester
+
+
+# XRD wavelengths in angstroms
+WAVELENGTHS = {
+    "CuKa": 1.54184,
+    "CuKa2": 1.54439,
+    "CuKa1": 1.54056,
+    "CuKb1": 1.39222,
+    "MoKa": 0.71073,
+    "MoKa2": 0.71359,
+    "MoKa1": 0.70930,
+    "MoKb1": 0.63229,
+    "CrKa": 2.29100,
+    "CrKa2": 2.29361,
+    "CrKa1": 2.28970,
+    "CrKb1": 2.08487,
+    "FeKa": 1.93735,
+    "FeKa2": 1.93998,
+    "FeKa1": 1.93604,
+    "FeKb1": 1.75661,
+    "CoKa": 1.79026,
+    "CoKa2": 1.79285,
+    "CoKa1": 1.78896,
+    "CoKb1": 1.63079,
+    "AgKa": 0.560885,
+    "AgKa2": 0.563813,
+    "AgKa1": 0.559421,
+    "AgKb1": 0.497082,
+}
+
+with open(os.path.join(os.path.dirname(__file__),
+                       "atomic_scattering_params.json")) as f:
+    ATOMIC_SCATTERING_PARAMS = json.load(f)
+
+
+def cart2sphere(cart_coord):
+    x = cart_coord[0]
+    y = cart_coord[1]
+    z = cart_coord[2]
+    r = math.sqrt(x**2 + y**2 + z**2) 
+    theta = math.acos(z/r) / math.pi
+    phi = (math.atan2(y, x) + math.pi) / (2 * math.pi)
+    return [r, theta, phi]
+
+
+class XRDCalculator(AbstractDiffractionPatternCalculator):
+    def __init__(self, wavelength="CuKa"):
+        if isinstance(wavelength, float):
+            self.wavelength = wavelength
+        else:
+            self.wavelength = WAVELENGTHS[wavelength]
+
+    def get_pattern(self, structure):
+        # deprecated function
+        pass
+
+    def get_npoint(self, structure):
+        
+        wavelength = self.wavelength
+        latt = structure.lattice
+        max_r = 2. / wavelength
+        recip_latt = latt.reciprocal_lattice_crystallographic
+        recip_basis = recip_latt.matrix
+        npoint = len(recip_latt.get_points_in_sphere([[0, 0, 0]], [0, 0, 0], max_r))
+        return npoint
+
+    def get_atomic_form_factor(self, structure):
+        # returns an (N x 123) array where N is the number of valid hkl points within the limiting sphere
+        out = []
+
+        wavelength = self.wavelength
+        latt = structure.lattice
+
+        # Obtained from Bragg condition. Note that reciprocal lattice
+        # vector length is 1 / d_hkl.
+        max_r = 2. / wavelength
+
+        # Obtain crystallographic reciprocal lattice points within range
+        recip_latt = latt.reciprocal_lattice_crystallographic
+        recip_basis = recip_latt.matrix
+        recip_pts = recip_latt.get_points_in_sphere(
+            [[0, 0, 0]], [0, 0, 0], max_r)
+        # Convert from Cartesian to spherical coordinate
+        recip_pts_spherical = []
+        for rpoint in recip_pts:
+            hkl = rpoint[0]
+            g_hkl = rpoint[1]
+            if (g_hkl < 1e-8): continue # skip [0 0 0] point
+            spherical = cart2sphere(np.dot(recip_basis.T, hkl))
+            recip_pts_spherical.append([hkl, g_hkl, spherical])
+
+        zs = []
+        coeffs = []
+        fcoords = []
+        occus = []
+
+        for site in structure:
+            for sp, occu in site.species.items():
+                try:
+                    c = ATOMIC_SCATTERING_PARAMS[sp.symbol]
+                except KeyError:
+                    raise ValueError("Unable to calculate XRD pattern as "
+                                     "there is no scattering coefficients for"
+                                     " %s." % sp.symbol)
+                zs.append(sp.Z)
+                coeffs.append(c)
+                fcoords.append(site.frac_coords)
+                occus.append(occu)
+
+        zs = np.array(zs)
+        coeffs = np.array(coeffs)
+        fcoords = np.array(fcoords)
+        occus = np.array(occus)
+
+        for hkl, g_hkl, spherical in sorted(
+                recip_pts_spherical, key=lambda i: (i[2][0], i[2][1], i[2][2])):
+            if g_hkl != 0:
+                # Bragg condition
+                theta = math.asin(wavelength * g_hkl / 2)
+
+                s = g_hkl / 2
+                s2 = s ** 2
+
+                g_dot_r = np.dot(fcoords, np.transpose([hkl])).T[0]
+
+                fs = zs - 41.78214 * s2 * np.sum(
+                    coeffs[:, :, 0] * np.exp(-coeffs[:, :, 1] * s2), axis=1)
+
+                atomic_form_factor = [0.] * 120
+                f_hkl = fs * occus * np.exp(2j * math.pi * g_dot_r)
+                i_hkl = (f_hkl * f_hkl.conjugate()).real
+                for idx in range(zs.size):
+                    atomic_intensity = i_hkl[idx]
+                    z = zs[idx]
+                    atomic_form_factor[z] += atomic_intensity
+                # normalize atomic form factor vector
+                atomic_form_factor /= np.linalg.norm(atomic_form_factor)
+                
+                # each point in the reciprocal space is represented by a 123D vector -- [x, y, z, atomic_form_factors]
+                hkl_data = spherical.copy()
+                hkl_data.extend(atomic_form_factor)
+                
+                out.append(hkl_data)
+
+        return out
+
+
+    def get_intensity(self, structure):
+        # returns an (N x 4) array where N is the number of valid hkl points within the limiting sphere
+        out = []
+
+        wavelength = self.wavelength
+        latt = structure.lattice
+
+        # Obtained from Bragg condition. Note that reciprocal lattice
+        # vector length is 1 / d_hkl.
+        max_r = 2. / wavelength
+
+        # Obtain crystallographic reciprocal lattice points within range
+        recip_latt = latt.reciprocal_lattice_crystallographic
+        recip_basis = recip_latt.matrix
+        recip_pts = recip_latt.get_points_in_sphere(
+            [[0, 0, 0]], [0, 0, 0], max_r)
+        # Convert from Cartesian to spherical coordinate
+        recip_pts_spherical = []
+        for rpoint in recip_pts:
+            hkl = rpoint[0]
+            g_hkl = rpoint[1]
+            if (g_hkl < 1e-8): continue # skip [0 0 0] point
+            spherical = cart2sphere(np.dot(recip_basis.T, hkl))
+            recip_pts_spherical.append([hkl, g_hkl, spherical])
+
+        zs = []
+        coeffs = []
+        fcoords = []
+        occus = []
+
+        for site in structure:
+            for sp, occu in site.species.items():
+                try:
+                    c = ATOMIC_SCATTERING_PARAMS[sp.symbol]
+                except KeyError:
+                    raise ValueError("Unable to calculate XRD pattern as "
+                                     "there is no scattering coefficients for"
+                                     " %s." % sp.symbol)
+                zs.append(sp.Z)
+                coeffs.append(c)
+                fcoords.append(site.frac_coords)
+                occus.append(occu)
+
+        zs = np.array(zs)
+        coeffs = np.array(coeffs)
+        fcoords = np.array(fcoords)
+        occus = np.array(occus)
+
+        for hkl, g_hkl, spherical in sorted(
+                recip_pts_spherical, key=lambda i: (i[2][0], i[2][1], i[2][2])):
+            if g_hkl != 0:
+                # Bragg condition
+                theta = math.asin(wavelength * g_hkl / 2)
+
+                s = g_hkl / 2
+                s2 = s ** 2
+
+                g_dot_r = np.dot(fcoords, np.transpose([hkl])).T[0]
+
+                fs = zs - 41.78214 * s2 * np.sum(
+                    coeffs[:, :, 0] * np.exp(-coeffs[:, :, 1] * s2), axis=1)
+
+                f_hkl = np.sum(fs * occus * np.exp(2j * math.pi * g_dot_r))
+                i_hkl = (f_hkl * f_hkl.conjugate()).real
+                
+                # each point in the reciprocal space is represented by a 4D vector -- [x, y, z, intensity]
+                hkl_data = spherical.copy()
+                hkl_data.append(i_hkl)
+                
+                out.append(hkl_data)
+
+        return out
+
+
+if __name__ == "__main__":
+    #xrd = XRDCalculator("CuKa")
+    xrd = XRDCalculator(5.0)
+
+    my_API_key = "gxTAyXSm2GvCdWer"
+    m = MPRester(api_key=my_API_key)
+    mp_data = m.query(criteria={"material_id": { "$eq" : "mp-2885" }}, properties=['cif', 'band_gap'])
+    
+    struct = Structure.from_str(mp_data[0]['cif'], fmt='cif')
+    #pattern = xrd.get_intensity(struct)
+    pattern = xrd.get_atomic_form_factor(struct)
