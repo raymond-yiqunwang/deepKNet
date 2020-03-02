accae106179dadd4c135f342677562d91da8c345
RaymondWang <raymondwang@u.northwestern.edu>
2020-03-02 09:45:57 -0600

    minor

--- deepKNet/model.py
+++ deepKNet/model.py
@@ -5,6 +5,43 @@ import torch.nn.functional as F
 class deepKNet(nn.Module):
     def __init__(self):
         super(deepKNet, self).__init__()
+        ## TF settings
+        # net = tf.keras.layers.Conv1D(128, 1, activation=tf.nn.relu)(feat)
+        # net = tf.keras.layers.Conv1D(256, 3, padding="same", activation=tf.nn.relu)(net)
+        # for i in range(0, 5):
+        #   intmp = net
+        #   net = tf.keras.layers.Conv1D(256, 3, padding="same", dilation_rate=2**i, activation=None)(net)
+        #   net = tf.keras.layers.BatchNormalization()(net)
+        #   net += intmp # residue connection
+        #   net = tf.nn.relu(net)
+        #   
+        # net = tf.keras.layers.Conv1D(512, 3, padding="same", activation=tf.nn.relu)(net)
+        # for i in range(0, 5):
+        #   intmp = net
+        #   net = tf.keras.layers.Conv1D(512, 3, padding="same", dilation_rate=2**i, activation=None)    (net)
+        #   net = tf.keras.layers.BatchNormalization()(net)
+        #   net += intmp # residue connection
+        #   net = tf.nn.relu(net)
+        #
+        # net = tf.keras.layers.Conv1D(1024, 3, padding="same", activation=tf.nn.relu)(net)
+        # for i in range(0, 5):
+        #   intmp = net
+        #   net = tf.keras.layers.Conv1D(1024, 3, padding="same", dilation_rate=2**i, activation=None    )(net)
+        #   net = tf.keras.layers.BatchNormalization()(net)
+        #   net += intmp # residue connection
+        #   net = tf.nn.relu(net)
+        #
+        # net = tf.math.reduce_max(net, axis=1)
+        # net = tf.reshape(net, [-1, 1024])
+        # net = tf_util.fully_connected(net, 512, bn=False, is_training=is_training,
+        #           scope='fc1', bn_decay=bn_decay)
+        # net = tf_util.fully_connected(net, 256, bn=False, is_training=is_training,
+        #           scope='fc2', bn_decay=bn_decay)
+        # net = tf_util.fully_connected(net, 64, bn=False, is_training=is_training,
+        #           scope='fc3', bn_decay=bn_decay)
+        # y_pred = tf_util.fully_connected(net, 1, activation_fn=None, scope='fc4')
+
+
         self.conv1 = torch.nn.Conv1d(97, 64, 1)
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.conv3 = torch.nn.Conv1d(128, 1024, 1)
