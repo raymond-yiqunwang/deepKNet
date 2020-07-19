60be3de8980f9c77dbff346bb63ddf143e48c0f5
RaymondWang <raymondwang@u.northwestern.edu>
2020-07-18 21:26:00 -0500

    minor

--- deepKNet/model.py
+++ deepKNet/model.py
@@ -7,25 +7,22 @@ class LeNet5(nn.Module):
         super(LeNet5, self).__init__()
         self.classification = classification
         # conv1
-        self.conv1 = nn.Conv2d(21, 64, kernel_size=5,stride=1, padding=2)
+        self.conv1 = nn.Conv2d(21, 64, kernel_size=7,stride=1, padding=3)
         self.bn1 = nn.BatchNorm2d(64)
         self.pooling1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         # conv2
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=7, stride=1, padding=3)
         self.bn2 = nn.BatchNorm2d(128)
         self.pooling2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        # conv3
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=5, stride=1, padding=2)
-        self.bn3 = nn.BatchNorm2d(256)
         # FC layers
         self.fc1 = nn.Linear(256, 128)
-        self.bn4 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(128)
         self.fc2 = nn.Linear(128, 64)
-        self.bn5 = nn.BatchNorm1d(64)
+        self.bn4 = nn.BatchNorm1d(64)
         if self.classification:
             self.fc3 = nn.Linear(64, 2)
             self.logsoftmax = nn.LogSoftmax(dim=1)
-            self.dropout = nn.Dropout(p=0.25)
+            self.dropout = nn.Dropout(p=0.5)
         else:
             self.fc3 = nn.Linear(64, 1)
 
@@ -36,17 +33,16 @@ class LeNet5(nn.Module):
         net = F.relu(self.bn2(self.conv2(net)))
         net = self.pooling2(net)
         
-        net = F.relu(self.bn3(self.conv3(net)))
-
         # pooling multi-views
-        net = torch.max(net, dim=1, keepdim=True)[0]
+        #net = torch.max(net, dim=1, keepdim=True)[0]
+        net = torch.mean(net, dim=1, keepdim=True)
 
         net = net.view(image.shape[0], -1)
-        net = F.relu(self.bn4(self.fc1(net)))
+        net = F.relu(self.bn3(self.fc1(net)))
         if self.classification:
             net = self.dropout(net)
         
-        net = F.relu(self.bn5(self.fc2(net)))
+        net = F.relu(self.bn4(self.fc2(net)))
        
         out = self.fc3(net)
         if self.classification:
@@ -55,3 +51,5 @@ class LeNet5(nn.Module):
         return out
 
 
+#class ResNet(nn.Module):
+    
