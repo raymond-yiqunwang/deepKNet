fixed random rotation in Xsys cls

data_gen/aggregate_data.py
@@ -71,14 +71,14 @@ def generate_dataset(xrd_data, topo_data, out_dir):
                           header=header_target, index=False, mode='w')
 
 
-def main():
-
+if __name__ == "__main__":
     # safeguard
     print("Attention, all existing training data will be deleted and regenerated..")
     
     # read xrd raw data
-    out_dir = "./raw_data/data_pointnet/"
-    xrd_file = "./raw_data/compute_xrd.csv"
+    out_dir = "./raw_data/data_Xsys27/"
+    xrd_file = "./raw_data/compute_xrd_Xsys27.csv"
+
     if not os.path.isfile(xrd_file):
         print("{} file does not exist, please generate it first..".format(xrd_file))
         sys.exit(1)
@@ -107,32 +107,5 @@ def main():
         pool.join()
         cnt += xrd_data.shape[0]
         print('finished processing {} materials'.format(cnt))
-
-
-def check_npoint(wavelength='CuKa'):
-    xrd_file = './raw_data/compute_xrd.csv'
-    data_all = pd.read_csv(xrd_file, sep=';', header=0, index_col=None, chunksize=100)
-    npoints = []
-    for idx, xrd_data in enumerate(data_all):
-        feat_len = xrd_data['features'].apply(ast.literal_eval).apply(len)
-        feat_len_list = feat_len.tolist()
-        print('Chunk min: {}, max: {}, mean: {}, median: {}, std: {}'.format(
-               np.min(feat_len_list), np.max(feat_len_list), np.mean(feat_len_list),
-               np.median(feat_len_list), np.std(feat_len_list)))
-        npoints += feat_len_list
-
-    print('Total min: {}, max: {}, mean: {}, median: {}, std: {}'.format(
-           np.min(npoints), np.max(npoints), np.mean(npoints),
-           np.median(npoints), np.std(npoints)))
-
-
-if __name__ == "__main__":
-    if True:
-        main()
-
-    if False:
-        # CuKa:
-        #   min: 50, max: 53000, mean: 4000, median: 3000, std: 4000
-        check_npoint(wavelength='CuKa')
 
 

data_gen/custom_MPdata.py
@@ -11,6 +11,7 @@ from multiprocessing import Pool
 from collections import defaultdict
 from pymatgen.core.structure import Structure
 from pymatgen.core.periodic_table import Element
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
 """ properties
         "material_id", "icsd_ids",
@@ -179,13 +180,104 @@ def customize_data(raw_data):
     return data_custom
 
 
-def main():
-    filename = "./raw_data/fetch_MPdata.csv"
-    if not os.path.isfile(filename):
-        print("{} file does not exist, please generate it first..".format(filename))
+def check_crystal_system(custom_data):
+    print('total size:', custom_data.shape[0])
+    print(custom_data['crystal_system'].value_counts())
+    drop_list =[]
+    for idx, irow in custom_data.iterrows():
+        struct = Structure.from_str(irow['cif'], fmt="cif")
+        # try until find a match
+        try:
+            sga = SpacegroupAnalyzer(struct, symprec=0.1)
+            assert(sga.get_crystal_system() == irow['crystal_system'])
+        except:
+            print('0.1 failed', irow['material_id'], 'added to drop list')
+            print(sga.get_crystal_system(), irow['crystal_system'])
+            drop_list.append(idx)
+            continue
+        # get conventional cell
+        conventional_struct = sga.get_conventional_standard_structure()
+        latt = conventional_struct.lattice.matrix
+        a, b, c = latt[0], latt[1], latt[2]
+        lena = np.linalg.norm(a)
+        lenb = np.linalg.norm(b)
+        lenc = np.linalg.norm(c)
+        theta_ab = np.arccos(np.dot(a,b)/(lena*lenb))/np.pi*180
+        theta_bc = np.arccos(np.dot(b,c)/(lenb*lenc))/np.pi*180
+        theta_ac = np.arccos(np.dot(a,c)/(lena*lenc))/np.pi*180
+        if irow['crystal_system'] == 'hexagonal' or \
+           irow['crystal_system'] == 'trigonal':
+            try:
+                assert(abs(lena - lenb) < 1E-1)
+                assert(abs(theta_ab-120) < 1E-1)
+                assert(abs(theta_bc-90) < 1E-1)
+                assert(abs(theta_ac-90) < 1E-1)
+            except:
+                print(irow['material_id'])
+                print('hexagonal or trigonal')
+                print(lena, lenb, lenc, theta_ab, theta_bc, theta_ac)
+        elif irow['crystal_system'] == 'cubic':
+            try:
+                assert(abs(lena - lenb) < 1E-1)
+                assert(abs(lenb - lenc) < 1E-1)
+                assert(abs(lena - lenc) < 1E-1)
+                assert(abs(theta_ab-90) < 1E-1)
+                assert(abs(theta_bc-90) < 1E-1)
+                assert(abs(theta_ac-90) < 1E-1)
+            except:
+                print(irow['material_id'])
+                print('cubic')
+                print(lena, lenb, lenc, theta_ab, theta_bc, theta_ac)
+        elif irow['crystal_system'] == 'tetragonal':
+            try:
+                assert(abs(lena - lenb) < 1E-1)
+                assert(abs(theta_ab-90) < 1E-1)
+                assert(abs(theta_bc-90) < 1E-1)
+                assert(abs(theta_ac-90) < 1E-1)
+            except:
+                print(irow['material_id'])
+                print('tetragonal')
+                print(lena, lenb, lenc, theta_ab, theta_bc, theta_ac)
+        elif irow['crystal_system'] == 'orthorhombic':
+            try:
+                assert(abs(theta_ab-90) < 1E-1)
+                assert(abs(theta_bc-90) < 1E-1)
+                assert(abs(theta_ac-90) < 1E-1)
+            except:
+                print(irow['material_id'])
+                print('orthorhombic')
+                print(lena, lenb, lenc, theta_ab, theta_bc, theta_ac)
+        elif irow['crystal_system'] == 'monoclinic':
+            try:
+                assert(abs(theta_ab-90) < 1E-1 and abs(theta_bc-90) < 1E-1) 
+            except:
+                print(irow['material_id'])
+                print('monoclinic')
+                print(lena, lenb, lenc, theta_ab, theta_bc, theta_ac)
+        else:
+            try:
+                assert(irow['crystal_system'] == 'triclinic')
+            except:
+                print(irow['material_id'])
+                print('UNK --', irow['crystal_system'])
+                print(lena, lenb, lenc, theta_ab, theta_bc, theta_ac)
+    
+    print('total number of entries to drop:', len(drop_list))
+    data_out = custom_data.drop(drop_list)
+    print('size with matched crystal system:', data_out.shape[0])
+    return data_out
+        
+
+if __name__ == "__main__":
+
+    input_file = "./raw_data/fetch_Xsys_data.csv"
+    out_file = "./raw_data/custom_Xsys_data.csv"
+
+    if not os.path.isfile(input_file):
+        print("{} file does not exist, please generate it first..".format(input_file))
         sys.exit(1)
     # get raw data from Materials Project
-    raw_data = pd.read_csv(filename, sep=';', header=0, index_col=None)
+    raw_data = pd.read_csv(input_file, sep=';', header=0, index_col=None)
 
     # show statistics of raw data
     print('\nShowing raw data:')
@@ -198,13 +290,12 @@ def main():
     print('\nShowing customized data:')
     show_statistics(data=data_custom, plot=False)
 
+    if True:
+        # only for crystal system classification
+        data_custom = check_crystal_system(data_custom)
+
     # write customized data
-    out_file = "./raw_data/custom_MPdata.csv"
     data_custom.to_csv(out_file, sep=';', columns=None, mode='w', \
                        header=data_custom.columns, index=None)
-
-
-if __name__ == "__main__":
-    main()
-
+    
 

deepKNet/data.py
@@ -68,7 +68,7 @@ class deepKNetDataset(Dataset):
 
         # apply random 3D rotation for data augmentation
         if self.data_aug:
-            alpha, beta, gamma = np.pi * np.random.random(3)
+            alpha, beta, gamma = 0.25 * np.pi * np.random.random(3)
             rot_matrix = [
                 np.cos(alpha)*np.cos(beta),
                 np.cos(alpha)*np.sin(beta)*np.sin(gamma) - np.sin(alpha)*np.cos(gamma),
@@ -126,9 +126,9 @@ class deepKNetDataset(Dataset):
         # 7-class
         elif self.target == 'crystal_system':
             cryst_sys_dict = {
-                'cubic': 0, 'hexagonal': 1, 'tetragonal': 2,
-                'trigonal': 3, 'orthorhombic': 4, 
-                'monoclinic': 5, 'triclinic': 6
+                'cubic': 0, 'orthorhombic': 1, 'tetragonal': 2,
+                'hexagonal': 3, 'trigonal': 3, 
+                'monoclinic': 4, 'triclinic': 5
             }
             prop = torch.Tensor([cryst_sys_dict[crystal_system]])
         # elasticity

misc/split_data.py
@@ -3,6 +3,20 @@ import pandas as pd
 import numpy as np
 import random
 
+def split_Xsys(root, out_file, train_ratio, valid_ratio, test_ratio):
+    file_names = [fname.split('.')[0] for fname in os.listdir(root) \
+                  if fname.split('.')[-1] == 'csv']
+    random.shuffle(file_names)
+    train_split = int(np.floor(len(file_names) * train_ratio))
+    valid_split = train_split + int(np.floor((len(file_names) * valid_ratio)))
+    train_data = file_names[:train_split]
+    valid_data = file_names[train_split:valid_split]
+    test_data = file_names[valid_split:]
+    out = [train_data, valid_data, test_data]
+    out = pd.DataFrame(out).transpose()
+    out.to_csv(out_file, header=['train', 'valid', 'test'], index=False)
+
+
 def split_gap(out_file, train_ratio, valid_ratio, test_ratio):
     data_root = "../data_gen/raw_data/data_pointnet/"
     file_names = [fname.split('.')[0] for fname in os.listdir(data_root) \
@@ -49,6 +63,15 @@ def split_elastic(out_file, train_ratio, valid_ratio, test_ratio):
 
 
 if __name__ == "__main__":
+    # crystal system
+    if True:
+        root = "../data_gen/raw_data/data_Xsys3/"
+        out_file = "Xsys_split1.csv"
+        train_ratio = 0.7
+        valid_ratio = 0.15
+        test_ratio = 0.15
+        split_Xsys(root, out_file, train_ratio, valid_ratio, test_ratio)
+
     # band gap
     if False:
         out_file = "gap_splitx.csv"
@@ -66,7 +89,7 @@ if __name__ == "__main__":
         split_topo(out_file, train_ratio, valid_ratio, test_ratio)
 
     # elastic
-    if True:
+    if False:
         out_file = "elastic_split5.csv"
         train_ratio = 0.7
         valid_ratio = 0.15